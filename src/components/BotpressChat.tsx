a572fc94e549c3f67fd37d76839b3da01d96c504
kenzinho1 <HaruCodeTI@users.noreply.github.com>
2025-08-04 11:12:00 -0400

    Corrigindo API do Botpress webchat para versão 3.1.0

--- src/components/BotpressChat.tsx
+++ src/components/BotpressChat.tsx
@@ -3,39 +3,39 @@
 import { useState } from 'react';
 import {
   Webchat,
-  WebchatProvider,
   Fab,
-  getClient,
-  Configuration,
+  useWebchat,
 } from '@botpress/webchat';
 
 const clientId = "023bdfde-0f4d-4ca7-8de4-eb5ecebeef54";
 
-const configuration: Configuration = {
-  color: '#7b61ff',
-};
-
 export function BotpressChat() {
-  const client = getClient({
+  const [isWebchatOpen, setIsWebchatOpen] = useState(false);
+
+  const { client, messages, participants, isTyping, error, clientState } = useWebchat({
     clientId,
   });
-
-  const [isWebchatOpen, setIsWebchatOpen] = useState(false);
 
   const toggleWebchat = () => {
     setIsWebchatOpen((prevState) => !prevState);
   };
 
+  if (error) {
+    console.error('Webchat error:', error);
+    return null;
+  }
+
   return (
-    <WebchatProvider client={client} configuration={configuration}>
+    <>
       <Fab onClick={toggleWebchat} />
-      <div
-        style={{
-          display: isWebchatOpen ? 'block' : 'none',
-        }}
-      >
-        <Webchat />
-      </div>
-    </WebchatProvider>
+      {isWebchatOpen && (
+        <Webchat
+          clientId={clientId}
+          configuration={{
+            color: '#7b61ff',
+          }}
+        />
+      )}
+    </>
   );
 } 
