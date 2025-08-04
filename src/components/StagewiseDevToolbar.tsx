ff4fcd6639d460fb0b277337426a4dd98699ac0f
kenzinho1 <HaruCodeTI@users.noreply.github.com>
2025-08-04 11:44:12 -0400

    Removendo arquivo StagewiseDevToolbar.tsx restante

--- src/components/StagewiseDevToolbar.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import ReactPlugin from "@stagewise-plugins/react";
-import { StagewiseToolbar } from "@stagewise/toolbar-next";
-
-export function StagewiseDevToolbar() {
-  return <StagewiseToolbar config={{ plugins: [ReactPlugin] }} />;
-}
